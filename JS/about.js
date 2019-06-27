953f089dbfdcc10cee39248fc7daf608e2b6d566
DJ Coleman <DJ@DJs-MacBook-Pro.local>
2019-06-26 20:28:49 -0700

    Adding tabs components

---
+++ JS/about.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+// let allImages = document.querySelectorAll('.carousel .slide')
+
+// let theIndex = 0
+
+// let slideTiming = setInterval(slideShow, 6000)
+
+
+// function slideShow() {
+//   allImages[theIndex].className = 'slide image-noshow'
+//   theIndex = (theIndex + 1) % allImages.length
+//   allImages[theIndex].classList = 'slide image-show'
+// }
