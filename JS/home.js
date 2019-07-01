c2838ab8dd37cc7e6a42845b9dfc09b23006cef5
Benjamin <benjamindoyle97@gmail.com>
2019-06-30 18:37:00 -0700

    responsiveness added

--- JS/home.js
+++ JS/home.js
@@ -3,10 +3,33 @@ let albumImageContainer = document.querySelectorAll('.album-image');
 let albumImage = document.querySelectorAll('.album-image img');
 let albumContainer = document.querySelector('.album-content');
 // changing image container width
-albumImageContainer.forEach((img) => {
-    img.style.width = `${(albumContainer.clientWidth - 40) / 3}px`;
-});
-// changing image width
-albumImage.forEach((img) => {
-    img.style.width = `${(albumContainer.clientWidth - 40) / 3}px`;
-});
+let changeImg = () => {
+    if (window.innerWidth > 1000) {
+        albumImageContainer.forEach((img) => {
+            img.style.width = `${(albumContainer.clientWidth - 40) / 3}px`;
+        });
+        // changing image width
+        albumImage.forEach((img) => {
+            img.style.width = `${(albumContainer.clientWidth - 40) / 3}px`;
+        });
+    }
+    else if (window.innerWidth < 1000 && window.innerWidth > 500) {
+        albumImageContainer.forEach((img) => {
+            img.style.width = `${(albumContainer.clientWidth - 40) / 2}px`;
+        });
+        // changing image width
+        albumImage.forEach((img) => {
+            img.style.width = `${(albumContainer.clientWidth - 40) / 2}px`;
+        });
+    }
+    else {
+        albumImageContainer.forEach((img) => {
+            img.style.width = `${(albumContainer.clientWidth - 40)}px`;
+        });
+        // changing image width
+        albumImage.forEach((img) => {
+            img.style.width = `${(albumContainer.clientWidth - 40)}px`;
+        });
+    }
+}
+changeImg();
