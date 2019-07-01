f1c49b901e88ce67cc51e6a43ed9fe9bcf8d170c
Benjamin <benjamindoyle97@gmail.com>
2019-06-30 19:54:40 -0700

    project complete

--- JS/nav.js
+++ JS/nav.js
@@ -5,7 +5,7 @@ let scrollPosition = 0;
 window.addEventListener('scroll', () => {
     scrollPosition = window.scrollY;
     if (scrollPosition > 75) {
-        navBar.style.padding = '20px 0'
+        navBar.style.padding = '25px 0'
     }
     else {
         navBar.style.padding = '25px 0'
